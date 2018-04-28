3ee3535dec9c97f2c08d0b3722346b918adde103
dendavidov <mail@dendavidov.com>
2018-04-28 14:42:01 +0300

    Simplify

--- src/task2.js
+++ src/task2.js
@@ -1,9 +1,12 @@
 const R = require('ramda');
 
-const checkAndSend = callBack =>
-  R.memoizeWith(R.identity, id => {
-    callBack(id);
-  });
+// const checkAndSend = callBack =>
+//   R.memoizeWith(R.identity, id => {
+//     callBack(id);
+//   });
+
+// и еще проще:
+const checkAndSend = callBack => R.memoizeWith(R.identity, callBack);
 
 module.exports = {
   checkAndSend,
