55c141884692bd50ac5b0316019a075981f95bea
dendavidov <mail@dendavidov.com>
2018-04-28 14:44:31 +0300

    Improve tests

--- src/task2.test.js
+++ src/task2.test.js
@@ -24,9 +24,12 @@ describe('checkAndSend', () => {
     sendById('1488');
     sendById('1489');
     sendById('1489');
+    sendById('1489');
+    sendById('1490');
 
-    expect(callBack.mock.calls.length).toBe(2);
+    expect(callBack.mock.calls.length).toBe(3);
     expect(callBack.mock.calls[0][0]).toBe('1488');
     expect(callBack.mock.calls[1][0]).toBe('1489');
+    expect(callBack.mock.calls[2][0]).toBe('1490');
   });
 });
